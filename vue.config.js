5e54f2c3c50fe42c32d15e309af2affd9f9c167b
AlexandreBecavin <62850530+AlexandreBecavin@users.noreply.github.com>
2021-02-11 21:29:37 +0100

    test pwa 21h29-11-02-2021

--- vue.config.js
+++ vue.config.js
@@ -50,4 +50,14 @@ module.exports = {
     ]
   },
 
+  pwa: {
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: 'service-worker.js',
+        // ...other Workbox options...
+    }
+  }
+
 }
